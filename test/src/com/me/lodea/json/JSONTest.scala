9b5deeb83825254627fb3a4bbeb6b72ba05401d4
quelgar <lodea@mac.com>
2008-10-22 11:25:55 +1100

    Silence warnings in test code

--- test/src/com/me/lodea/json/JSONTest.scala
+++ test/src/com/me/lodea/json/JSONTest.scala
@@ -8,25 +8,25 @@ object JSONTest {
   def main(args: Array[String]) {
     val result = JSON.parse(new InputStreamReader(new FileInputStream(args(0))))
     val json = result.get
-    json match {
+    (json: @unchecked) match {
       case JSONObject(values) => {
         printf("Object: %s%n", values)
-        values("an int") match {
+        (values("an int"): @unchecked) match {
           case JSONNumber(666) => println("Matched integer 666!")
         }
-        values("an int") match {
+        (values("an int"): @unchecked) match {
           case JSONNumber(667) => error("Should not match")
           case _ => println("Didn't match 667")
         }
-        values("Description") match {
+        (values("Description"): @unchecked) match {
           case JSONString(x) => printf("Description = '%s'%n", x)
         }
-        values("a decimal") match {
+        (values("a decimal"): @unchecked) match {
           case JSONNumber(42.666) => println("Matched 42.666") 
         }
-        values("Image") match {
+        (values("Image"): @unchecked) match {
           case JSONObject(imageValues) => {
-            imageValues("IDs") match {
+            (imageValues("IDs"): @unchecked) match {
               case JSONArray(List(JSONNumber(116), JSONNumber(x), _*)) => printf("Matched IDs list, 2nd value = %d%n", x.toInt)
             }
           }
